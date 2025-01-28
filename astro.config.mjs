01084e0ccc1130d8b665b807a74960bc4ae5e94d
354073823@qq.com <354073823@qq.com>
2025-01-28 19:40:03 +0800

    更新home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,19 @@
 import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind';
+import path from 'path'
 
 export default defineConfig({
   site: 'https://erpanomer.github.io',
   base: '/',
+  integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('src')
+      }
+    }
+  },
+  experimental: {
+    svg: true,
+  },
 })
