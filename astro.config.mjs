4bb710fe0ee59af4fe335c65e67b961d3cfb3637
354073823@qq.com <354073823@qq.com>
2025-01-30 23:41:40 +0800

    更新home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,6 +38,6 @@ export default defineConfig({
   },
   output: 'server',
   adapter: node({
-    mode: 'standalone'
+    mode: 'middleware'
   })
 })
---
+++ server.mjs
@@ -0,0 +1,11 @@
+import express from 'express';
+import { handler as ssrHandler } from './dist/server/entry.mjs';
+
+const app = express();
+// 根据 astro.config.mjs 中的 `base` 选项进行更改。
+// 它们应该匹配。默认值为"/"。
+const base = '/';
+app.use(base, express.static('./dist/client/'));
+app.use(ssrHandler);
+
+app.listen(9000);
