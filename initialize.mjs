586cc3a1e04ecd8568e9ee8f1ae6b260cfe59a49
354073823@qq.com <354073823@qq.com>
2025-02-01 18:35:19 +0800

    更新home page

--- initialize.mjs
+++ initialize.mjs
@@ -1,4 +1,8 @@
+import { exec } from 'child_process'
+
+
 export async function handler (context) {
+  exec('ln -sfn /opt/nodejs/node_modules /code/node_modules');
   console.log('initializer');
   return "";
 }
