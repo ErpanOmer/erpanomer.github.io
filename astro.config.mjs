ba86a83786dc985b022fbf6c12f8e0229b0b915c
ErpanOmer <354073823@qq.com>
2025-12-25 21:31:59 +0800

    update web site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, passthroughImageService } from 'astro/config'
 import tailwind from '@astrojs/tailwind';
 import path from 'path'
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://erpanomer.github.io',
+  site: 'https://erpanomer.nurverse.com',
   base: '/',
+  // SSR Mode
   output: 'server',
-  adapter: cloudflare(),
+
+  // Use 'compile' service to optimize static images at build time
+  // This works even in SSR mode for assets known at build time
+  adapter: cloudflare({
+    imageService: 'passthrough',
+  }),
+  image: {
+    service: passthroughImageService()
+  },
 
   integrations: [tailwind(), sitemap({
     serialize(item) {
