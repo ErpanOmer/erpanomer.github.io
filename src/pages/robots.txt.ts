8f8539fcb0e96bb29314cc775500ab9112a50448
ErpanOmer <354073823@qq.com>
2026-01-07 21:21:08 +0800

    robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,7 +3,10 @@ import type { APIRoute } from 'astro';
 const getRobotsTxt = (sitemapURL: URL) => `
 User-agent: *
 Allow: /
+
 Sitemap: ${sitemapURL.href}
+Sitemap: https://erpanomer.nurverse.com/projects/leetcode/sitemap.xml
+Sitemap: https://erpanomer.nurverse.com/projects/learning/sitemap.xml
 `;
 
 export const GET: APIRoute = ({ site }) => {
