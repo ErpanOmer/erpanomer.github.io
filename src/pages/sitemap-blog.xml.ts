0cf8c2a02c7cb6c103396d768b47c745c8bb6c2c
ErpanOmer <354073823@qq.com>
2026-01-19 18:14:25 +0800

    robots.txt

--- src/pages/sitemap-blog.xml.ts
+++ src/pages/sitemap-blog.xml.ts
@@ -3,6 +3,19 @@ import { projects } from "@/data/projects";
 
 const SITE_URL = "https://erpanomer.nurverse.com";
 
+function escapeXml(unsafe: string): string {
+    return unsafe.replace(/[<>&'"]/g, (c) => {
+        switch (c) {
+            case '<': return '&lt;';
+            case '>': return '&gt;';
+            case '&': return '&amp;';
+            case '\'': return '&apos;';
+            case '"': return '&quot;';
+            default: return c;
+        }
+    });
+}
+
 export async function GET() {
     const blogEntries = await getCollection("blog", ({ data }) => {
         return !data.draft;
@@ -12,13 +25,13 @@ export async function GET() {
         (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
     );
 
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>`;
-    xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+    xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">`;
 
     // Blog Posts
     for (const post of sortedBlogEntries) {
         xml += `<url>`;
-        xml += `<loc>${SITE_URL}/blog/${post.slug}</loc>`;
+        xml += `<loc>${escapeXml(`${SITE_URL}/blog/${post.slug}`)}</loc>`;
         xml += `<lastmod>${(post.data.updatedDate || post.data.pubDate).toISOString()}</lastmod>`;
         xml += `</url>`;
     }
@@ -33,7 +46,7 @@ export async function GET() {
                 }
 
                 xml += `<url>`;
-                xml += `<loc>${SITE_URL}${project.link}</loc>`;
+                xml += `<loc>${escapeXml(`${SITE_URL}${project.link}`)}</loc>`;
                 xml += `<lastmod>${new Date().toISOString()}</lastmod>`;
                 xml += `</url>`;
             }
@@ -42,10 +55,10 @@ export async function GET() {
 
     xml += `</urlset>`;
 
-    return new Response(xml, {
+    return new Response(xml.trim(), {
         status: 200,
         headers: {
-            "Content-Type": "application/xml",
+            "Content-Type": "application/xml; charset=utf-8",
         },
     });
 }
